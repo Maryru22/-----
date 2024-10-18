c0e4fb0e968141cbc1e8a38517df064d1e8f4845
Maryru22 <mariyarudacova98@gmail.com>
2024-10-18 17:26:10 +0300

    лаба2.1

--- 2.java
+++ 2.java
@@ -4,18 +4,16 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Запрашиваем у пользователя число
         System.out.print("Введите число для вывода таблицы умножения: ");
         int number = scanner.nextInt();
 
-        // Выводим таблицу умножения
         System.out.println("Таблица умножения для числа " + number + ":");
+        
         for (int i = 1; i <= 10; i++) {
             int result = number * i;
-            System.out.println(number + " * " + i + " = " + result);
+            System.out.println(number + " x " + i + " = " + result);
         }
 
-        // Закрываем сканер
         scanner.close();
     }
 }
